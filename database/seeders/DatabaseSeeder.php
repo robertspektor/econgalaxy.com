16aba8f1f56a47d2a1893a3b4364f6fe535b50d4
Robert Spektor <r.spektor@bernstein-badshop.de>
2024-10-30 23:41:44 +0100

    deactivate sector seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             FactionSeeder::class,
-            SectorsTableSeeder::class,
-            JumpGatesTableSeeder::class,
+//            SectorsTableSeeder::class,
+//            JumpGatesTableSeeder::class,
             MegaCorporationSeeder::class,
             EchoCovertWeaponSeeder::class,
             UnionDefenseWeaponSeeder::class,
